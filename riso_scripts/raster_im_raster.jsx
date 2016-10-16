e12c5c1c97c2d784bc2d8b8c4cbd1c57af2a6995
Josef Renner <josef.renner@gmail.com>
2016-10-17 00:48:04 +0200

    Bring everything to first working version

--- riso_scripts/raster_im_raster.jsx
+++ riso_scripts/raster_im_raster.jsx
@@ -6,94 +6,72 @@
  *
  * To Do:
  * - es gibt nur ein grösseres Feld – was, wenn es mehrere wären?
- * - alles ist links oben angeschlagen. es bräuchte eine verschiebung in x/y die der hälfte des übrigen platzes entspricht
- *
- * Fehlerquelle: Als hauptsächliche Fehlerquelle haben sich Zeile 23f erwiesen: Bei berechnen der Anzahl Felder auf einer Zeile gab es immer wieder Fälle, wo die errechnete Anzahl nicht mit der gezeichneten Anzahl übereinstimmte.
- * Durch den Abzug der Hälfte eines Feldes von der Gesamtbreite/-höhe scheint das Problem vorerst zu verschwinden.
- * Besser wäre es, der Diskrepanz auf den Grund zu gehen: Loop Zeile 32 ist nicht kongruent mit Zeile 25f.
- * Es wöre besser, den Loop mit ganzen Zahlen zu durchlaufen und auf den Variablen fieldsH und fieldsV aufzubauen.
- * Der Vektor müsste beim Zeichnen jeweils mit den Dimensionen der Felder mutlipliziert werden.
  */
-
-
 
 function draw() {
   b.units(b.PT);
 
-  var rasterFieldWidth = 36;
-  var rasterFieldHeight = 24;
+  // basic grid field dimensions
+  var rasterFieldWidth = 48;
+  var rasterFieldHeight = 64;
 
-  // how can the number of fileds be calculated without mistake?
+  // number of fields in horizontal and vertical direction
   var fieldsH = Math.round( ( b.width - ( rasterFieldWidth / 2 ) ) / rasterFieldWidth );
-  b.println( fieldsH );
   var fieldsV = Math.round( ( b.height - ( rasterFieldHeight / 2 ) ) / rasterFieldHeight );
+  // b.println( "Number of fields, horizontally: " + fieldsH );
+  // b.println( "Number of fields, vertically: " + fieldsV );
+
+  // total number of fields
   var fieldsTotal = fieldsH * fieldsV;
 
+  // total width and height of the basic grid
+  var rasterWidth = fieldsH * rasterFieldWidth;
+  var rasterHeight = fieldsV * rasterFieldHeight;
+  b.println( rasterWidth );
+  b.println( rasterHeight );
+
+  // center on page (see b.translate below)
+  var offsetH = ( b.width - rasterWidth ) / 2;
+  var offsetV = ( b.height - rasterHeight ) / 2;
+
+  // array to store the basic grid
   var raster1coords = [];
 
-  for ( var y = 0; y < b.height - rasterFieldHeight; y += rasterFieldHeight ) {
-    for ( var x = 0; x < b.width - rasterFieldWidth; x += rasterFieldWidth ) {
+  // store vector for every field in basic grid
+  for ( var y = 0; y < fieldsV; y++ ) {
+    for ( var x = 0; x < fieldsH; x++ ) {
       raster1coords.push( new b.Vector( x, y ) );
     }
   }
 
-  var scaleFactor_h = 3;
-  var scaleFactor_v = 4;
+  // multiply the vectors to actual size
+  for ( var i = 0; i < raster1coords.length; i++ ) {
+    raster1coords[ i ].x = raster1coords[ i ].x * rasterFieldWidth;
+    raster1coords[ i ].y = raster1coords[ i ].y * rasterFieldHeight;
+  }
+
+  // define larger field that is painted on top of the basic grid
+  var scaleFactor_h = 4;
+  var scaleFactor_v = 6;
   var raster2fieldWidth = scaleFactor_h * rasterFieldWidth;
   var raster2fieldHeight = scaleFactor_v * rasterFieldHeight;
+
+  // array to store larger field(s) in
   var raster2coords = [];
 
-  function indexOfSomewhere( arr, fact_x, fact_y, numX, numY ) { // array of smaller raster, scale factor of bigger raster, number of rasterfields horizontally and vertically
-    var possibleX = numX - ( fact_x - 1 ); // enough space to draw bigger fields?
-    var possibleY = numY - ( fact_y - 1 );
-    var allowedIndexes = [];
-    var forbiddenIndexes = [];
-    var allowedIndex = -1;
-
-    for ( var i = 0; i < numX * ( possibleY ); i++ ) { // subtract ( fact - 1 ) to eclude bottom area where there’s not enough space
-          allowedIndexes.push( i );
-        }    
-
-    // allowed range: numX - ( fact - 1 )
-    for ( var i = allowedIndexes.length - 1; i >= 0; i-- ) { // iterate through all fields
-      if ( i % numX >= numX - ( fact_x - 1 ) ) {
-          forbiddenIndexes.unshift( i );
-          allowedIndexes.splice( i, 1 );
-      }
-    }
-
-    b.println( "forbidden indexes:" );
-    b.println( forbiddenIndexes );
-    // b.println( "allowed indexes:" );
-    // b.println( allowedIndexes );
-    
-    var randIndex = Math.round( b.random( 0, allowedIndexes.length ) );
-    allowedIndex = allowedIndexes[ randIndex ];
-    return allowedIndex;
-  }
-
+  // define point in basic grid where larger field will be drawn (see function indexOfBiggerField)
   indexOfBiggerField = indexOfSomewhere( raster1coords, scaleFactor_h, scaleFactor_v, fieldsH, fieldsV );
+
+  // store origin of larger field in array (there might be more at some point)
   raster2coords.push( raster1coords[ indexOfBiggerField ] );
-  // we need to splice all fields in the smaller raster that would be covered by the bigger field
-  // splice needs to start from behind otherwise the raster will turn all ahoo
-  // we need to get rid of Math.sqrt( scaleFactor ) number of fields
-  // we start at indexOfBiggerField + fieldsV * scaleFactor + scaleFactor (last Field to be spliced)
 
-  function findCoveredFields( ind, row, fact_x, fact_y ) {
-    var arr = [];
-    for ( var i = 0; i < fact_y; i++ ) {
-      for ( var j = 0; j < fact_x; j++ ) {
-        arr.push( ind + ( row * i ) + j );
-      }
-    }
-    return arr;
-  }
-
+  // Find and store indexes that will be deleted in array (see function findCoveredFields below)
   var indexesToDelete = findCoveredFields( indexOfBiggerField, fieldsH, scaleFactor_h, scaleFactor_v );
+  // b.println( "indexes to delete: " indexesToDelete );
 
-  b.println( "indexes to delete:" );
-  b.println( indexesToDelete );
-
+  // All fields in the smaller raster that would be covered by the bigger field
+  // need to be spliced from array that stores the basic grid.
+  // Splicing needs to start from behind, otherwise the raster will turn all ahoo.
   for ( var i = indexesToDelete.length - 1; i >= 0; i-- ) {
     raster1coords.splice( indexesToDelete[ i ], 1 );
   }
@@ -101,15 +79,61 @@ function draw() {
   // b.textSize( rasterFieldHeight / 2 );
   b.textSize( 6 );
   b.textFont( "Input" );
+  b.translate( offsetH, offsetV );
+
+  // draw basic grid
   for ( var i = 0; i < raster1coords.length; i++ ) {
     // b.rect( raster1coords[ i ].x, raster1coords[ i ].y, rasterFieldWidth, rasterFieldHeight );
     b.text( i, raster1coords[ i ].x, raster1coords[ i ].y, rasterFieldWidth, rasterFieldHeight  )
   }
-  
+
+  // draw larger field
   for ( var i = 0; i < raster2coords.length; i++ ) {
     b.rect( raster2coords[ i ].x, raster2coords[ i ].y, raster2fieldWidth, raster2fieldHeight );
   }
 
+}
+
+// Find an arbitrary origin for the larger field we want to draw.
+// It should not move out of the basic grid
+function indexOfSomewhere( arr, fact_x, fact_y, numX, numY ) { // array of smaller raster, scale factor of bigger raster, number of rasterfields horizontally and vertically
+  var possibleX = numX - ( fact_x - 1 ); // enough space to draw bigger fields?
+  var possibleY = numY - ( fact_y - 1 );
+  var allowedIndexes = [];
+  var forbiddenIndexes = [];
+  var allowedIndex = -1;
+
+  for ( var i = 0; i < numX * ( possibleY ); i++ ) { // subtract ( fact - 1 ) to eclude bottom area where there’s not enough space
+        allowedIndexes.push( i );
+      }
+
+  // allowed range: numX - ( fact - 1 )
+  for ( var i = allowedIndexes.length - 1; i >= 0; i-- ) { // iterate through all fields
+    if ( i % numX >= numX - ( fact_x - 1 ) ) {
+        forbiddenIndexes.unshift( i );
+        allowedIndexes.splice( i, 1 );
+    }
+  }
+
+  // b.println( "forbidden indexes:" );
+  // b.println( forbiddenIndexes );
+  // b.println( "allowed indexes:" );
+  // b.println( allowedIndexes );
+
+  var randIndex = Math.round( b.random( 0, allowedIndexes.length ) );
+  allowedIndex = allowedIndexes[ randIndex ];
+  return allowedIndex;
+}
+
+// Identify fields in basic grid that are covered by larger field
+function findCoveredFields( ind, row, fact_x, fact_y ) {
+  var arr = [];
+  for ( var i = 0; i < fact_y; i++ ) {
+    for ( var j = 0; j < fact_x; j++ ) {
+      arr.push( ind + ( row * i ) + j );
+    }
+  }
+  return arr;
 }
 
 b.go();
